a48d91926aba6a5a45fda82f071229583bce94e1
Prasenjit Paul <89773002+prasenjit1113@users.noreply.github.com>
2023-05-25 13:13:10 +0530

    Update topbar.php

--- admin/topbar.php
+++ admin/topbar.php
@@ -16,11 +16,11 @@
   		
   		</div>
       <div class="col-md-4 float-left text-white">
-        <large><b><?php echo isset($_SESSION['system']['name']) ? $_SESSION['system']['name'] : '' ?></b></large>
+        <large><b><h5><?php echo isset($_SESSION['system']['name']) ? $_SESSION['system']['name'] : '' ?></h5></b></large>
       </div>
 	  	<div class="float-right">
         <div class=" dropdown mr-4">
-            <a href="#" class="text-white dropdown-toggle"  id="account_settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['login_name'] ?> </a>
+            <a href="#" class="text-white dropdown-toggle"  id="account_settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i><b><?php echo $_SESSION['login_name'] ?> </b></i></a>
               <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
                 <a class="dropdown-item" href="javascript:void(0)" id="manage_my_account"><i class="fa fa-cog"></i> Manage Account</a>
                 <a class="dropdown-item" href="ajax.php?action=logout"><i class="fa fa-power-off"></i> Logout</a>
